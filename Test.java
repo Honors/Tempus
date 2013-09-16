66f4499ef495680dc0fe0926797fc1db5d3c0d21
Matt Neary <neary.matt@gmail.com>
2013-09-16 08:57:51 -0400

    shortened day test

--- Test.java
+++ Test.java
@@ -11,9 +11,22 @@ public class Test extends Examen {
     Break[] breaks = {};
     Schedule sched = new Schedule();
     Float[] sorted = sched.reshape(segments, breaks, 443f);
-    return inRange(sorted[2], 44, 45);
+    return sorted[2] == 45;
+  }  
+ public Boolean testShortDay() {
+    Block hr = new Block(7, false);
+    Block n = new Block(45, true);
+    Block l = new Block(77, false);
+    Block s = new Block(5, false);
+    Block a = new Block(2, false);
+    Block[] segments = {hr, s, n, s, n, s, n, s, n, a, s, l, s, n, s, n, a, s, n};
+    Break[] breaks = {};
+    Schedule sched = new Schedule();
+    Float[] sorted = sched.reshape(segments, breaks, 355f);
+    return inRange(sorted[2], 32, 35);
   }  
   public static void main(String[] args) {
     Examen.run(Test.class, "testDefault", "Matches the default school schedule.");
+    Examen.run(Test.class, "testShortDay", "Matches the default short school schedule.");
   }
 }
