88fa6e7ad99f551f372ed5f3b03f8d47df3ce5de
Griffin Maxwell <griffin@maxwell-family.net>
2013-09-16 08:25:26 -0400

    correct Homeroom/class durations

--- Test.java
+++ Test.java
@@ -2,15 +2,15 @@ public class Test extends Examen {
  public Boolean testDefault() {
     // form a list of blocks and breaks and form
     // a schedule of them to fit a given duration.
-    Block hr = new Block(5, false);
-    Block n = new Block(42, true);
+    Block hr = new Block(7, false);
+    Block n = new Block(45, true);
     Block l = new Block(77, false);
     Block s = new Block(5, false);
     Block a = new Block(2, false);
     Block[] segments = {hr, s, n, s, n, s, n, s, n, a, s, l, s, n, s, n, a, s, n};
     Break[] breaks = {};
     Schedule sched = new Schedule();
-    Float[] sorted = sched.reshape(segments, breaks, 438f);
+    Float[] sorted = sched.reshape(segments, breaks, 443f);
     return inRange(sorted[2], 44, 45);
   }  
   public static void main(String[] args) {
