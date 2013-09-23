6e70618e080b2f8ed73bc08fbbf3d9a10b46b515
Matt Neary <neary.matt@gmail.com>
2013-09-23 09:09:51 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,24 @@
+# Tempus Server
+
+##Dependencies
+- Java `>=1.6`
+- [Conscript](https://github.com/n8han/conscript)
+- [giter8](https://github.com/n8han/giter8/)
+
+##Build & Run
+Run the Scala Build Tool with the following.
+
+```sh
+$ ./sbt.sh
+```
+
+Then, from within its interactive shell, run the following.
+
+```sh
+$ ./sbt.sh
+> ~;container:start; container:reload /
+```
+
+##Source
+The main classes are located at `/src/main/scala/com/mattneary/tempus`.
+
---
+++ src/main/scala/com/mattneary/tempus/TempusServlet.scala
@@ -0,0 +1,30 @@
+package com.mattneary.tempus
+
+import org.scalatra._
+import scalate.ScalateSupport
+import scalaj.http.{Http, HttpOptions}
+
+import scala.io.Source
+import java.io._
+import org.slf4j.{Logger, LoggerFactory}
+import java.nio._
+import net.liftweb.json._
+import net.liftweb.json.JsonDSL._
+import java.io.{File,FileInputStream,FileOutputStream}
+
+class TempusServlet extends TempusStack {
+  val dest = "/Users/mattneary/Desktop/imgurious/"
+  val file = "index.html"
+  def renderPage(file: String) {
+    org.scalatra.util.io.copy(new FileInputStream(file), response.getOutputStream)
+  }
+
+  get("/") {
+    renderPage(dest + file)
+  }
+  get("/asset/*") {
+    val rest = multiParams("splat")
+    val file = rest.mkString("/")
+    renderPage(dest + file)
+  }
+}
